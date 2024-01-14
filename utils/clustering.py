f0d9f6595d1f7864d784b70a952431225bf626ad
wind_mta <tuanphongmta0205@gmail.com>
2024-01-14 17:32:00 +0900

    [Sprint01] Add AHC function

---
+++ utils/clustering.py
@@ -0,0 +1,47 @@
+import numpy as np
+from sklearn.cluster import AgglomerativeClustering
+
+def ah_clustering_dense(pointcloud):
+    '''
+    Agglomerative hierarchical clustering (AHC)
+
+    '''
+    #-------------
+    # REPARE
+    #-------------
+    # Get Pointcloud Data
+    pointcloud = np.fromfile(pointcloud, dtype=np.float32).reshape(-1, 5)
+
+    # Get label from input Pointcloud   
+    label = pointcloud[:, 4]
+  
+
+    # Get points labeled as "noise"                                      
+    nx, ny, nz = pointcloud[(label == 1), :3].T 
+    # noise_points = np.array([nx]).T
+    noise_points = np.array([nx, ny, nz]).T
+
+    # Check error
+    assert  ((nx.shape[0] == len(noise_points))  & \
+             (ny.shape[0] == len(noise_points))  & \
+             (nz.shape[0] == len(noise_points))) , \
+            "[ERR][ah_clustering_dense][PREPARE] Incompatible shape"
+
+    # Iterative parameter
+    num_points = len(noise_points)                                                                 
+
+    #-------------
+    # AHC
+    #-------------
+    ahc = AgglomerativeClustering(n_clusters=2, linkage='ward')
+    labels = ahc.fit(noise_points).labels_
+
+    # Check error
+    assert  ((nx.shape[0] == labels.shape[0])  & \
+             (ny.shape[0] == labels.shape[0])  & \
+             (nz.shape[0] == labels.shape[0])) , \
+            "[ERR][ah_clustering_dense][AHC] Incompatible shape"
+
+    nx_group_1 = [nx[x] for x in range(num_points) if labels[x] == 1]
+    nx_group_2 = [nx[x] for x in range(num_points) if labels[x] == 0]
+    return labels, nx_group_1, nx_group_2

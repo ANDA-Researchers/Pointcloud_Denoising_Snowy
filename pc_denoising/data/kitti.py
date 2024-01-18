4e778ccef0bef7ca42182a6bca7fd9741a7641c2
hphnngcquan <hphnngcquan@github.com>
2024-01-18 18:01:37 +0900

    Add _voxel_partition

--- pc_denoising/data/kitti.py
+++ pc_denoising/data/kitti.py
@@ -7,7 +7,12 @@ from torch.utils.data import Dataset
 
 
 class KITTI(Dataset):
-    def __init__(self, root_dir: Path, info_path: Path, is_reduced: bool = True) -> None:
+    _range = np.array([[0.0, 70.4], [-40.0, 40.0], [-3.0, 1.0]])
+    _scale = np.array([0.2, 0.2, 0.4])
+
+    def __init__(
+        self, root_dir: Path, info_path: Path, is_reduced: bool = True
+    ) -> None:
         super().__init__()
         infos = pickle.loads(info_path.read_bytes())
         lidar_dir_name = "velodyne_reduced" if is_reduced else "velodyne"
@@ -20,8 +25,18 @@ class KITTI(Dataset):
         return len(self._lidar_paths)
 
     def __getitem__(self, idx: int):
-        return _load_lidar(self._lidar_paths[idx])
+        lidar = _load_lidar(self._lidar_paths[idx])
+        voxel_coords = _voxel_partition(lidar[:, :3], self._range[:, 0], self._scale)
+        return voxel_coords
 
 
 def _load_lidar(lidar_path: Path) -> npt.NDArray[np.float32]:
-    return np.fromfile(lidar_path, dtype=np.float32).astype(np.float32).reshape(-1, 5)
+    return np.fromfile(lidar_path, dtype=np.float32).reshape(-1, 5)
+
+
+def _voxel_partition(
+    voxel_coords: npt.NDArray[np.float32],
+    offset: npt.NDArray[np.float32],
+    scale: npt.NDArray[np.float32],
+) -> npt.NDArray[np.int32]:
+    return ((voxel_coords - offset) / scale)[:, [2, 0, 1]].astype(np.int32)

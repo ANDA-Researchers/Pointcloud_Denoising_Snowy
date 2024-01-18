1395113f3b7a82e1a428f1a1dd21add5bf6dfe9f
hphnngcquan <hphnngcquan@github.com>
2024-01-18 20:31:28 +0900

    Create _partition_voxels, sample_points, _group_points

--- pc_denoising/data/kitti.py
+++ pc_denoising/data/kitti.py
@@ -1,5 +1,7 @@
 import pickle
+import random
 from pathlib import Path
+from typing import List, Sequence, Tuple
 
 import numpy as np
 import numpy.typing as npt
@@ -7,8 +9,9 @@ from torch.utils.data import Dataset
 
 
 class KITTI(Dataset):
-    _range = np.array([[0.0, 70.4], [-40.0, 40.0], [-3.0, 1.0]])
-    _scale = np.array([0.2, 0.2, 0.4])
+    _lidar_range = np.array([[0.0, 70.4], [-40.0, 40.0], [-3.0, 1.0]])
+    _voxel_size = np.array([0.2, 0.2, 0.4])
+    _num_points_per_voxel = 35
 
     def __init__(
         self, root_dir: Path, info_path: Path, is_reduced: bool = True
@@ -26,17 +29,42 @@ class KITTI(Dataset):
 
     def __getitem__(self, idx: int):
         lidar = _load_lidar(self._lidar_paths[idx])
-        voxel_coords = _voxel_partition(lidar[:, :3], self._range[:, 0], self._scale)
-        return voxel_coords
+        np.random.shuffle(lidar)
+        voxel_coords, voxel_indices = self._partition_voxels(lidar[:, :3])
+        point_groups = _group_points(lidar, voxel_coords, voxel_indices)
+        sampled_points = self._sample_points(point_groups)
+        voxel_features = sampled_points[:, :, 3]
+        return voxel_coords, voxel_features
+
+    def _partition_voxels(
+        self, voxel_coords: npt.NDArray[np.float32]
+    ) -> Tuple[npt.NDArray[np.int64], npt.NDArray[np.int64]]:
+        voxel_coords = (voxel_coords - self._lidar_range[:, 0]) // self._voxel_size
+        return np.unique(voxel_coords[:, [2, 0, 1]], return_inverse=True, axis=0)
+
+    def _sample_points(
+        self,
+        lidar: npt.NDArray[np.float32],
+        voxel_coords: npt.NDArray[np.int64],
+        voxel_indices: npt.NDArray[np.int64],
+    ):
+        num_voxels = len(voxel_coords)
+        sampled_points = np.zeros(
+            (num_voxels, self._num_points_per_voxel, 7), dtype=np.float32
+        )
+        for idx in range(num_voxels):
+            points_in_voxel = lidar[voxel_indices == idx]
+            num_sampled_points = min(self._num_points_per_voxel, len(points_in_voxel))
+            sampled_points[idx, :num_sampled_points]
 
 
 def _load_lidar(lidar_path: Path) -> npt.NDArray[np.float32]:
-    return np.fromfile(lidar_path, dtype=np.float32).reshape(-1, 5)
+    return np.fromfile(lidar_path, dtype=np.float32).reshape(-1, 4)
 
 
-def _voxel_partition(
-    voxel_coords: npt.NDArray[np.float32],
-    offset: npt.NDArray[np.float32],
-    scale: npt.NDArray[np.float32],
-) -> npt.NDArray[np.int32]:
-    return ((voxel_coords - offset) / scale)[:, [2, 0, 1]].astype(np.int32)
+def _group_points(
+    lidar: npt.NDArray[np.float32],
+    voxel_coords: npt.NDArray[np.int64],
+    voxel_indices: npt.NDArray[np.int64],
+) -> List[npt.NDArray[np.float32]]:
+    return [lidar[voxel_indices == idx] for idx, _ in enumerate(voxel_coords)]

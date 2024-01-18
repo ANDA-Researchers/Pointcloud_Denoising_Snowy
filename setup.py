122aedc986a2e174429ee69c60231c3c866b7cb6
hphnngcquan <hphnngcquan@github.com>
2024-01-18 16:28:53 +0900

    Add ipykernel

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name="pc_denoising",
     version="0.0.1",
     install_requires=[
+        "ipykernel==6.29.0",
         "numpy==1.24.4",
         "torch==2.1.2",
         "torchvision==0.16.2",

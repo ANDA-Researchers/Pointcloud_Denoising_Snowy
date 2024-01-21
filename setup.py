727745965fb4742f892e766489ae56e631d5046a
vuong-viet-hung <vuongviethung156@gmail.com>
2024-01-21 17:31:21 +0900

    Add mayavi

--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ setup(
     install_requires=[
         "ipykernel==6.29.0",
         "matplotlib==3.7.4",
+        "mayavi==4.8.1",
         "numpy==1.24.4",
-        "opencv-python==4.9.0",
+        "opencv-python==4.9.0.80",
         "torch==2.1.2",
         "torchvision==0.16.2",
         "black==23.12.1",

e1dcb80fd794c2ea2d2cb9f540f4a3a2bcad9aad
vuong-viet-hung <vuongviethung156@gmail.com>
2024-01-21 19:53:17 +0900

    Rename voxel_net to voxelnet

--- pc_denoising/voxelnet/voxelnet.py
+++ pc_denoising/voxelnet/voxelnet.py
@@ -4,32 +4,39 @@ import torch
 from torch.autograd import Variable
 from config import config as cfg
 
+
 # conv2d + bn + relu
 class Conv2d(nn.Module):
-
-    def __init__(self,in_channels,out_channels,k,s,p, activation=True, batch_norm=True):
+    def __init__(
+        self, in_channels, out_channels, k, s, p, activation=True, batch_norm=True
+    ):
         super(Conv2d, self).__init__()
-        self.conv = nn.Conv2d(in_channels,out_channels,kernel_size=k,stride=s,padding=p)
+        self.conv = nn.Conv2d(
+            in_channels, out_channels, kernel_size=k, stride=s, padding=p
+        )
         if batch_norm:
             self.bn = nn.BatchNorm2d(out_channels)
         else:
             self.bn = None
         self.activation = activation
-    def forward(self,x):
+
+    def forward(self, x):
         x = self.conv(x)
         if self.bn is not None:
-            x=self.bn(x)
+            x = self.bn(x)
         if self.activation:
-            return F.relu(x,inplace=True)
+            return F.relu(x, inplace=True)
         else:
             return x
 
+
 # conv3d + bn + relu
 class Conv3d(nn.Module):
-
     def __init__(self, in_channels, out_channels, k, s, p, batch_norm=True):
         super(Conv3d, self).__init__()
-        self.conv = nn.Conv3d(in_channels, out_channels, kernel_size=k, stride=s, padding=p)
+        self.conv = nn.Conv3d(
+            in_channels, out_channels, kernel_size=k, stride=s, padding=p
+        )
         if batch_norm:
             self.bn = nn.BatchNorm3d(out_channels)
         else:
@@ -42,60 +49,62 @@ class Conv3d(nn.Module):
 
         return F.relu(x, inplace=True)
 
+
 # Fully Connected Network
 class FCN(nn.Module):
-
-    def __init__(self,cin,cout):
+    def __init__(self, cin, cout):
         super(FCN, self).__init__()
         self.cout = cout
         self.linear = nn.Linear(cin, cout)
         self.bn = nn.BatchNorm1d(cout)
 
-    def forward(self,x):
+    def forward(self, x):
         # KK is the stacked k across batch
         kk, t, _ = x.shape
-        x = self.linear(x.view(kk*t,-1))
+        x = self.linear(x.view(kk * t, -1))
         x = F.relu(self.bn(x))
-        return x.view(kk,t,-1)
+        return x.view(kk, t, -1)
+
 
 # Voxel Feature Encoding layer
 class VFE(nn.Module):
-
-    def __init__(self,cin,cout):
+    def __init__(self, cin, cout):
         super(VFE, self).__init__()
         assert cout % 2 == 0
         self.units = cout // 2
-        self.fcn = FCN(cin,self.units)
+        self.fcn = FCN(cin, self.units)
 
     def forward(self, x, mask):
         # point-wise feauture
         pwf = self.fcn(x)
-        #locally aggregated feature
-        laf = torch.max(pwf,1)[0].unsqueeze(1).repeat(1,cfg.T,1)
+        # locally aggregated feature
+        laf = torch.max(pwf, 1)[0].unsqueeze(1).repeat(1, cfg.T, 1)
         # point-wise concat feature
-        pwcf = torch.cat((pwf,laf),dim=2)
+        pwcf = torch.cat((pwf, laf), dim=2)
         # apply mask
         mask = mask.unsqueeze(2).repeat(1, 1, self.units * 2)
         pwcf = pwcf * mask.float()
 
         return pwcf
 
+
 # Stacked Voxel Feature Encoding
 class SVFE(nn.Module):
-
     def __init__(self):
         super(SVFE, self).__init__()
-        self.vfe_1 = VFE(7,32)
-        self.vfe_2 = VFE(32,128)
-        self.fcn = FCN(128,128)
+        self.vfe_1 = VFE(7, 32)
+        self.vfe_2 = VFE(32, 128)
+        self.fcn = FCN(128, 128)
+
     def forward(self, x):
-        mask = torch.ne(torch.max(x,2)[0], 0)
+        mask = torch.ne(torch.max(x, 2)[0], 0)
         x = self.vfe_1(x, mask)
         x = self.vfe_2(x, mask)
         x = self.fcn(x)
         # element-wise max pooling
-        x = torch.max(x,1)[0]
+        x = torch.max(x, 1)[0]
         return x
+
 
 # Convolutional Middle Layer
 class CML(nn.Module):
@@ -110,6 +119,7 @@ class CML(nn.Module):
         x = self.conv3d_2(x)
         x = self.conv3d_3(x)
         return x
+
 
 # Region Proposal Network
 class RPN(nn.Module):
@@ -127,14 +137,30 @@ class RPN(nn.Module):
         self.block_3 += [nn.Conv2d(256, 256, 3, 1, 1) for _ in range(5)]
         self.block_3 = nn.Sequential(*self.block_3)
 
-        self.deconv_1 = nn.Sequential(nn.ConvTranspose2d(256, 256, 4, 4, 0),nn.BatchNorm2d(256))
-        self.deconv_2 = nn.Sequential(nn.ConvTranspose2d(128, 256, 2, 2, 0),nn.BatchNorm2d(256))
-        self.deconv_3 = nn.Sequential(nn.ConvTranspose2d(128, 256, 1, 1, 0),nn.BatchNorm2d(256))
+        self.deconv_1 = nn.Sequential(
+            nn.ConvTranspose2d(256, 256, 4, 4, 0), nn.BatchNorm2d(256)
+        )
+        self.deconv_2 = nn.Sequential(
+            nn.ConvTranspose2d(128, 256, 2, 2, 0), nn.BatchNorm2d(256)
+        )
+        self.deconv_3 = nn.Sequential(
+            nn.ConvTranspose2d(128, 256, 1, 1, 0), nn.BatchNorm2d(256)
+        )
 
-        self.score_head = Conv2d(768, cfg.anchors_per_position, 1, 1, 0, activation=False, batch_norm=False)
-        self.reg_head = Conv2d(768, 7 * cfg.anchors_per_position, 1, 1, 0, activation=False, batch_norm=False)
+        self.score_head = Conv2d(
+            768, cfg.anchors_per_position, 1, 1, 0, activation=False, batch_norm=False
+        )
+        self.reg_head = Conv2d(
+            768,
+            7 * cfg.anchors_per_position,
+            1,
+            1,
+            0,
+            activation=False,
+            batch_norm=False,
+        )
 
-    def forward(self,x):
+    def forward(self, x):
         x = self.block_1(x)
         x_skip_1 = x
         x = self.block_2(x)
@@ -143,32 +169,21 @@ class RPN(nn.Module):
         x_0 = self.deconv_1(x)
         x_1 = self.deconv_2(x_skip_2)
         x_2 = self.deconv_3(x_skip_1)
-        x = torch.cat((x_0,x_1,x_2),1)
-        return self.score_head(x),self.reg_head(x)
+        x = torch.cat((x_0, x_1, x_2), 1)
+        return self.score_head(x), self.reg_head(x)
 
 
 class VoxelNet(nn.Module):
-
     def __init__(self):
         super(VoxelNet, self).__init__()
         self.svfe = SVFE()
         self.cml = CML()
         self.rpn = RPN()
 
-    def voxel_indexing(self, sparse_features, coords):
-        dim = sparse_features.shape[-1]
-
-        dense_feature = Variable(torch.zeros(dim, cfg.N, cfg.D, cfg.H, cfg.W).cuda())
-
-        dense_feature[:, coords[:,0], coords[:,1], coords[:,2], coords[:,3]]= sparse_features
-
-        return dense_feature.transpose(0, 1)
-
     def forward(self, voxel_features, voxel_coords):
-
         # feature learning network
         vwfs = self.svfe(voxel_features)
-        vwfs = self.voxel_indexing(vwfs,voxel_coords)
+        vwfs = voxel_indexing(vwfs, voxel_coords)
 
         # convolutional middle network
         cml_out = self.cml(vwfs)
@@ -176,7 +191,18 @@ class VoxelNet(nn.Module):
         # region proposal network
 
         # merge the depth and feature dim into one, output probability score map and regression map
-        psm,rm = self.rpn(cml_out.view(cfg.N,-1,cfg.H, cfg.W))
+        psm, rm = self.rpn(cml_out.view(cfg.N, -1, cfg.H, cfg.W))
 
         return psm, rm
 
+
+def voxel_indexing(sparse_features, coords):
+    dim = sparse_features.shape[-1]
+
+    dense_feature = Variable(torch.zeros(dim, cfg.N, cfg.D, cfg.H, cfg.W).cuda())
+
+    dense_feature[
+        :, coords[:, 0], coords[:, 1], coords[:, 2], coords[:, 3]
+    ] = sparse_features
+
+    return dense_feature.transpose(0, 1)

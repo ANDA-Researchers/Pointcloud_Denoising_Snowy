0fde17fb90a5aac34535084f178762bd8caae15d
vuong-viet-hung <vuongviethung156@gmail.com>
2024-01-24 22:50:14 +0900

    add ipywidgets and tqdm

--- setup.py
+++ setup.py
@@ -7,8 +7,10 @@ setup(
     packages=["pc_denoising"],
     install_requires=[
         "ipykernel==6.29.0",
+        "ipywidgets==8.1.1",
         "numpy==1.24.4",
         "matplotlib==3.7.2",
         "opencv-python==4.9.0.80",
+        "tqdm==4.66.1",
     ],
 )

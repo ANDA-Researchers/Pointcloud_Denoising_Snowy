185f0de79f74078bb6b95d0fc2b83240eb6324b6
vuong-viet-hung <vuongviethung156@gmail.com>
2024-01-26 22:23:09 +0900

    Fix imports

--- pc_denoising/main.py
+++ pc_denoising/main.py
@@ -1,7 +1,7 @@
 from pytorch_lightning.cli import LightningCLI
 
 from pc_denoising.data import KITTI
-from pc_denoising.model import MinkowskiUNet
+from pc_denoising.models import MinkowskiUNet
 
 
 def cli_main():

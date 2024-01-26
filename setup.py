81d0536cd81ebc21364c00ed4b3d3fd2d5203297
vuong-viet-hung <vuongviethung156@gmail.com>
2024-01-26 22:14:45 +0900

    Add scikit-learn

--- setup.py
+++ setup.py
@@ -9,9 +9,10 @@ setup(
         "ipykernel==6.29.0",
         "ipywidgets==8.1.1",
         "jsonargparse[signatures]>=4.27.2",
-        "numpy==1.24.4",
         "matplotlib==3.7.2",
+        "numpy==1.24.4",
         "opencv-python==4.9.0.80",
+        "scikit-learn==1.10.1",
         "tqdm==4.66.1",
     ],
 )
